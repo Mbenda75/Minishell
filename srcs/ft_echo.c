4d313547a1ab24ee8ec5853b1e287648e9fc9312
user42 <adnen.aloui2@hotmail.com>
2022-03-21 10:28:54 +0100

    0 marche avec les multiple $, bug avec ?$

--- srcs/ft_echo.c
+++ srcs/ft_echo.c
@@ -6,7 +6,7 @@
 /*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/16 22:01:53 by benmoham          #+#    #+#             */
-/*   Updated: 2022/03/21 08:53:59 by user42           ###   ########.fr       */
+/*   Updated: 2022/03/21 09:58:51 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,23 +59,39 @@ int ft_count_dollars(char **cmd, int i)
 int ft_echo_several_dollars(char **cmd, int i, t_env *echo_env)
 {
 	int k;
+	int j;
 	char **split_bydollar;
 
 	k = -1;
+	j = 0;
 	while (cmd[i][++k] != '$')
 		ft_putchar_fd(cmd[i][k], 1);
 	k = 0;
 	split_bydollar = ft_split(cmd[i], '$');
 	while (split_bydollar[k])
 	{
-		if (ft_checK_env_var_existence(split_bydollar[k]) == SUCCESS)
+		if (ft_strchr(split_bydollar[k], '?'))
 		{
-			split_bydollar[k] = ft_change_dollar_var(split_bydollar[k]);
-			ft_putstr_fd(split_bydollar[k], 1);
+			j = 1;
+			ft_putnbr_fd(g_list->exit_value, 1);
+			while (split_bydollar[k][j])
+			{
+				ft_putchar_fd(split_bydollar[k][j], 1);
+				j++;
+			}
+			k++;
 		}
 		else
-			ft_putstr_fd("", 1);
-		k++;
+		{
+			if (ft_checK_env_var_existence(split_bydollar[k]) == SUCCESS)
+			{
+				split_bydollar[k] = ft_change_dollar_var(split_bydollar[k]);
+				ft_putstr_fd(split_bydollar[k], 1);
+			}
+			else
+				ft_putstr_fd("", 1);
+			k++;
+		}
 	}
 	if (cmd[i + 1])
 		write(1, " ", 1);
@@ -103,18 +119,17 @@ int ft_echo_single_dollar(char **cmd, int i, t_env *echo_env)
 		j++;
 	}
 	split_bydollar = ft_split(cmd[i], '$');
-	/*if (split_bydollar[0] == NULL || split_bydollar[1] == NULL)
+	/*if (split_bydollar[1] == NULL)
 	{
 		ft_putstr_fd("$", 1);
 		if (cmd[i + 1])
 			write(1, " ", 1);
-		return (SUCCESS);
 	}*/
 	if (ft_strchr(split_bydollar[jump], '?'))
 	{
 		j = 1;
 		ft_putnbr_fd(g_list->exit_value, 1);
-		while (split_bydollar[jump][j] == '?')
+		while (split_bydollar[jump][j])
 		{
 			ft_putchar_fd(split_bydollar[jump][j], 1);
 			j++;
