e24e0736cb5b26205c1410b20cde018f46190ce2
adaloui <adnen.aloui2@hotmail.com>
2022-03-22 21:05:55 +0100

    plus de leaks sur echo, need encore norminette

--- srcs/utils_echo.c
+++ srcs/utils_echo.c
@@ -6,7 +6,7 @@
 /*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/22 18:59:16 by user42            #+#    #+#             */
-/*   Updated: 2022/03/22 20:30:42 by user42           ###   ########.fr       */
+/*   Updated: 2022/03/22 21:03:36 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -68,16 +68,28 @@ int ft_echo_several_dollars(char **cmd, int i, t_env *echo_env)
 	return (SUCCESS);
 }
 
-void	ft_echo_print_single_dollar(char **split_bydollar)
+t_decompte	ft_echo_print_single_dollar(char **cmd, int i,
+		char **split_bydollar, t_decompte j_et_s, int j)
 {
+	char *str;
 	
+	if (ft_checK_env_var_existence(split_bydollar[j_et_s.l]) == SUCCESS)
+	{
+		str = ft_change_dollar_var(split_bydollar[j_et_s.l]);
+		ft_putstr_fd(str, 1);
+		free(str);
+	}
+	else
+	{
+		j_et_s.i = 1;
+		ft_putstr_fd("", 1);
+	}
+	return (j_et_s);
 }
 
-int ft_echo_single_dollar(char **cmd, int i, t_env *echo_env)
+t_decompte ft_init_and_write(char **cmd, int i, t_decompte j_et_s)
 {
-	t_decompte j_et_s; // sert à sauter en cas de présence d'une écriture avant un dollar, on saute une case ex : echo popololo$user : jump sera égal à 1
 	int j;
-	char **split_bydollar;
 
 	j = 0;
 	j_et_s.l = 0; //jump à 0 dans le cas où y'a pas de caractère avant le $
@@ -90,6 +102,17 @@ int ft_echo_single_dollar(char **cmd, int i, t_env *echo_env)
 		j_et_s.j = 1;
 		j++;
 	}
+	return (j_et_s);
+}
+
+int ft_echo_single_dollar(char **cmd, int i, t_env *echo_env)
+{
+	t_decompte j_et_s; // sert à sauter en cas de présence d'une écriture avant un dollar, on saute une case ex : echo popololo$user : jump sera égal à 1
+	int j;
+	char **split_bydollar;
+
+	j = 0;
+	j_et_s = ft_init_and_write(cmd, i, j_et_s);
 	j = ft_strlen(cmd[i]) - 1;
 	if (cmd[i][j] == '$')
 	{
@@ -102,7 +125,6 @@ int ft_echo_single_dollar(char **cmd, int i, t_env *echo_env)
 	if (ft_strchr(split_bydollar[j_et_s.l], '?'))
 	{
 		j = 1;
-	
 		ft_putnbr_fd(g_list->exit_value, 1);
 		while (split_bydollar[j_et_s.l][j])
 		{
@@ -113,17 +135,8 @@ int ft_echo_single_dollar(char **cmd, int i, t_env *echo_env)
 			write(1, " ", 1);
 		return (SUCCESS);
 	}
-	split_bydollar[j_et_s.l] == ft_strjoin(split_bydollar[j_et_s.l], "=");
-	if (ft_checK_env_var_existence(split_bydollar[j_et_s.l]) == SUCCESS)
-	{
-		split_bydollar[j_et_s.l] = ft_change_dollar_var(split_bydollar[j_et_s.l]);
-		ft_putstr_fd(split_bydollar[j_et_s.l], 1);
-	}
-	else
-	{
-		j_et_s.i = 1;
-		ft_putstr_fd("", 1);
-	}
+	j_et_s = ft_echo_print_single_dollar(cmd, i, split_bydollar, j_et_s, j);
+	ft_free_charr(split_bydollar);
 	if ((cmd[i + 1] && j_et_s.i == 0) || (cmd[i + 1] && j_et_s.i == 1 && j_et_s.j == 1) )
 		write(1, " ", 1);
 	return (SUCCESS);
